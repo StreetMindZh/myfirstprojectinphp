400a493d1df9a8725899ca5a10f3a1e62549c5a3
Zlatko Jankovski <100656935+StreetMindZh@users.noreply.github.com>
2023-01-04 15:26:21 +0100

    all works

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<!--Start Footer-->        
+<div class="footer">
+
+(c) 2022 Developer StreetMindz.com "the pro art inc"         
+
+</div>
+<!--End Footer-->
---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<div class="menubar">
+        <h1>My Contact Book</h1>
+
+            <div class="myname">
+               <div class="avatar">Z</div> Zlatko Jankovski
+            </div>
+
+           
+</div>
+
