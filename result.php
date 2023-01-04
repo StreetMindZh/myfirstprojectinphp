15a518488e593075dc653768bda32f264c7146f6
Zlatko Jankovski <100656935+StreetMindZh@users.noreply.github.com>
2023-01-04 16:36:27 +0100

    till what colors script

---
+++ result.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+
+</head>
+<body>
+
+      <!--Start Integrate Header PHP -->
+
+    <?php include "includes/header.php"; ?>
+
+     <!--Start Google Fonts Ubuntu Link-->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+
+      <!--End Google Fonts Ubuntu Link-->
+
+        <!--Start Bootstrap CDN CSS and JavaScript Link-->
+        
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+      <!--End Bootstrap CDN CSS and JavaScript Link-->
+
+      <link rel="stylesheet" href="css/styles.css">
+
+
+    <!--End CSS Internal-->
+        <?php include "includes/footer.php"; ?>
+
+</body>
+</html>
