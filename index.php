38df8ac5beb80fca74535004470e0d9013d38819
Zlatko Jankovski <100656935+StreetMindZh@users.noreply.github.com>
2023-01-10 16:02:20 +0100

    CALL TO BACKGROUND COLOR HOVER IN CSS

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
     <!--End Google Fonts Ubuntu Link-->
 
     <!--Start Bootstrap CDN CSS and JavaScript Link-->
-
+ <!--
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
+-->
     <!--End Bootstrap CDN CSS and JavaScript Link-->
 
      <!--Start CSS External-->
@@ -120,7 +120,7 @@
                     <b>$name</b><br>
                     $phone
 
-
+                    <a class='phonebtn' href='tel:$phone'>CALL TO</a>
                 </div>
                 ";
             }
